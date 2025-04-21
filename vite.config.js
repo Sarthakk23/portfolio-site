03acffa7307cfc616a68cf34fe3c7709ec86b23a
Sarthakk23 <sarthak23492@iiitd.ac.in>
2025-04-21 23:17:17 +0530

    Add base path for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // ← add this line (replace with your actual repo name)
+  base: '/portfolio-site/',
   plugins: [react()],
 })
